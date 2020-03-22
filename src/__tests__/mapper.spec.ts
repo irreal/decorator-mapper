45e8073254bb644fcc43c295f47ffa7e69071aa6
irreal <milos.spasojevic.tms@gmail.com>
2020-03-22 23:10:00 +0100

    WIP mapper. apply not working?  still missing other direction tests and impl

--- src/__tests__/mapper.spec.ts
+++ src/__tests__/mapper.spec.ts
@@ -1,32 +1,57 @@
 import { Mapper } from "../mapper";
 import { field } from "../field";
+import { mapFunction } from "../mapFunction";
 
 class TestClass {
   @field("jsonName")
+  @field("customSourceName", "custom source")
   name: string = "";
   id: number = 0;
   entryDate?: Date = undefined;
   propWithNoMap: string = "";
+  @mapFunction()
+  customMapFunction(data: any, source: string) {
+    this.name = "overriden";
+    console.log('got called with', data, source);
+    // expect(data).toBeTruthy();
+    // expect(data.somethingElse).toEqual("else");
+    // expect(source).toEqual("default");
+  }
 }
 describe("mapper", () => {
-  it("returns empty default class with empty json", () => {
-    const mapped = Mapper.mapTo<TestClass>(TestClass, {});
-    expect(mapped).toBeTruthy();
-  });
-
-  it("assigns values without maps", () => {
-    const mapped = Mapper.mapTo<TestClass>(TestClass, {
-      propWithNoMap: "prop value"
+  describe("mapping from json", () => {
+    it("returns empty default class with empty json", () => {
+      const mapped = Mapper.mapTo<TestClass>(TestClass, {});
+      expect(mapped).toBeTruthy();
     });
-    expect(mapped).toBeTruthy();
-    expect(mapped.propWithNoMap).toEqual("prop value");
-  });
 
-  it("assigns values with maps", () => {
-    const mapped = Mapper.mapTo<TestClass>(TestClass, {
-      jsonName: "a name"
+    it("assigns values without maps", () => {
+      const mapped = Mapper.mapTo<TestClass>(TestClass, {
+        propWithNoMap: "prop value"
+      });
+      expect(mapped).toBeTruthy();
+      expect(mapped.propWithNoMap).toEqual("prop value");
     });
-    expect(mapped).toBeTruthy();
-    expect(mapped.name).toEqual("a name");
+
+    it("assigns values with maps", () => {
+      const mapped = Mapper.mapTo<TestClass>(TestClass, {
+        jsonName: "a name"
+      });
+      expect(mapped).toBeTruthy();
+      expect(mapped.name).toEqual("a name");
+    });
+
+    it("assigns values with maps with custom sources", () => {
+      const mapped = Mapper.mapTo<TestClass>(TestClass, {
+        customSourceName: "a name"
+      }, 'custom source');
+      expect(mapped).toBeTruthy();
+      expect(mapped.name).toEqual("a name");
+    });
+
+    it("calls custom mapping function if defined", () => {
+      const mapped = Mapper.mapTo<TestClass>(TestClass, { jsonName: "a name", somethingElse: "else" });
+      expect(mapped.name).toEqual("overriden");
+    });
   });
 });
--- src/mapper.ts
+++ src/mapper.ts
@@ -1,17 +1,22 @@
 import { getField } from "./field";
+import { getMapFunction, MapDirection } from "./mapFunction";
 
 export class Mapper {
-  static mapTo<T>(type: new () => T, data: any): T {
+  static mapTo<T>(type: new () => T, data: any, source = "default"): T {
     const instance = new type();
 
     Object.keys(instance).forEach(key => {
-      const mappedName = getField(type, key);
+      const mappedName = getField(type, key, source);
       if (mappedName) {
         instance[key] = data[mappedName];
         return;
       }
       if (data[key]) instance[key] = data[key];
     });
+    let customMapFunction = getMapFunction(type, MapDirection.FromJson, source);
+    if (customMapFunction) {
+      customMapFunction.apply([data, source], instance);
+    }
     return instance;
   }
 }
