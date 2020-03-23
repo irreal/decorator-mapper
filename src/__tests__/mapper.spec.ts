2902ac56a9dd1e74834c77adbd094c085ab18f22
Milos Spasojevic <milos.spasojevic.tms@gmail.com>
2020-03-23 12:36:13 +0100

    finished mapper implementation

--- src/__tests__/mapper.spec.ts
+++ src/__tests__/mapper.spec.ts
@@ -1,6 +1,7 @@
+// tslint:disable: max-classes-per-file
 import { Mapper } from "../mapper";
 import { field } from "../field";
-import { mapFunction } from "../mapFunction";
+import { mapFunction, MapDirection } from "../mapFunction";
 
 class TestClass {
   @field("jsonName")
@@ -23,13 +24,13 @@ class TestClass {
 describe("mapper", () => {
   describe("mapping from json", () => {
     it("returns empty default class with empty json", () => {
-      const mapped = Mapper.mapTo<TestClass>(TestClass, {});
+      const mapped = Mapper.mapFromJson<TestClass>(TestClass, {});
       expect(mapped).toBeTruthy();
       expect(mapped).toEqual(new TestClass());
     });
 
     it("assigns values without maps", () => {
-      const mapped = Mapper.mapTo<TestClass>(TestClass, {
+      const mapped = Mapper.mapFromJson<TestClass>(TestClass, {
         propWithNoMap: "prop value",
       });
       expect(mapped).toBeTruthy();
@@ -37,7 +38,7 @@ describe("mapper", () => {
     });
 
     it("assigns values with maps", () => {
-      const mapped = Mapper.mapTo<TestClass>(TestClass, {
+      const mapped = Mapper.mapFromJson<TestClass>(TestClass, {
         jsonName: "a name",
       });
       expect(mapped).toBeTruthy();
@@ -45,7 +46,7 @@ describe("mapper", () => {
     });
 
     it("assigns values with maps with custom sources", () => {
-      const mapped = Mapper.mapTo<TestClass>(
+      const mapped = Mapper.mapFromJson<TestClass>(
         TestClass,
         {
           customSourceName: "a name",
@@ -57,11 +58,86 @@ describe("mapper", () => {
     });
 
     it("calls custom mapping function if defined", () => {
-      const mapped = Mapper.mapTo<TestClass>(TestClass, {
+      const mapped = Mapper.mapFromJson<TestClass>(TestClass, {
         jsonName: "a name",
         somethingElse: "something else",
       });
       expect(mapped.propToOverride).toEqual("overriden");
+    });
+  });
+
+  class MapToJsonPlain {
+    name: string = "";
+    id: number = 0;
+  }
+  describe("mapping to json", () => {
+    it("maps non custom names", () => {
+      const instance = new MapToJsonPlain();
+      instance.name = "test name";
+      instance.id = 3;
+      const mapped = Mapper.mapToJson(instance);
+      expect(mapped).toEqual(instance);
+    });
+
+    class MapToJsonCustom {
+      @field("customName")
+      name: string = "";
+      id: number = 3;
+    }
+    it("maps custom names in default source", () => {
+      const instance = new MapToJsonCustom();
+      instance.name = "test name";
+      const mapped = Mapper.mapToJson(instance);
+      expect(mapped).toEqual(
+        expect.objectContaining({ customName: "test name", id: 3 })
+      );
+    });
+
+    class MapToJsonCustomSource {
+      @field("customName", "custom source")
+      name: string = "";
+    }
+
+    it("maps custom names from a custom source", () => {
+      const instance = new MapToJsonCustomSource();
+      instance.name = "test name";
+      const mapped = Mapper.mapToJson(instance, "custom source");
+      expect(mapped).toEqual({ customName: "test name" });
+    });
+
+    class MapToJsonCustomFunction {
+      name: string = "";
+      @mapFunction(MapDirection.ToJson)
+      customToJsonFunction(mappedJson: any, _: string) {
+        mappedJson.name = "overriden name";
+      }
+    }
+
+    it("calls custom mapping function if defined", () => {
+      const instance = new MapToJsonCustomFunction();
+      instance.name = "test name";
+      const mapped = Mapper.mapToJson(instance);
+      expect(mapped.name).toEqual("overriden name");
+    });
+
+    class MapToJsonCustomFunctionCustomSource {
+      name: string = "";
+
+      @mapFunction(MapDirection.ToJson)
+      customToJsonFunction(_: any, __: any) {
+        fail(new Error("called incorrect mapping function"));
+      }
+      @mapFunction(MapDirection.ToJson, "custom source")
+      customToJsonCustomSourceFunction(mappedJson: any, source: string) {
+        expect(source).toEqual("custom source");
+        mappedJson.name = "overriden";
+      }
+    }
+    it("calls custom mapping function for custom source", () => {
+      const instance = new MapToJsonCustomFunctionCustomSource();
+      instance.name = "test name";
+      const mapped = Mapper.mapToJson(instance, "custom source");
+      expect(mapped.name).toEqual("overriden");
     });
   });
 });
--- src/mapper.ts
+++ src/mapper.ts
@@ -2,7 +2,7 @@ import { getField } from "./field";
 import { getMapFunction, MapDirection } from "./mapFunction";
 
 export class Mapper {
-  static mapTo<T>(type: new () => T, data: any, source = "default"): T {
+  static mapFromJson<T>(type: new () => T, data: any, source = "default"): T {
     const instance = new type();
 
     Object.keys(instance).forEach((key) => {
@@ -22,5 +22,28 @@ export class Mapper {
       instance[customMapFunction](data, source);
     }
     return instance;
+  }
+
+  static mapToJson(instance: object, source = "default"): any {
+    const mapped = {};
+    Object.keys(instance).forEach((key) => {
+      const mappedName = getField(instance.constructor, key, source);
+      if (mappedName) {
+        mapped[mappedName] = instance[key];
+        return;
+      }
+      mapped[key] = instance[key];
+    });
+
+    const customMappingFunctionName = getMapFunction(
+      instance.constructor,
+      MapDirection.ToJson,
+      source
+    );
+    if (customMappingFunctionName) {
+      instance[customMappingFunctionName](mapped, source);
+    }
+
+    return mapped;
   }
 }
